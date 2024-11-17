efea7fd7c2675dbf9799d8b3f6434e7d23f033c2
Enzo <enzowederma@gmail.com>
2024-11-16 23:31:56 -0300

    🐞 fix: add missing section attribute

--- src/app/schemas/section.schemas.ts
+++ src/app/schemas/section.schemas.ts
@@ -5,12 +5,21 @@ export const SectionCreateRequestSchema = Zod.object({
         .string({ required_error: "Field name must compose request body." })
         .min(1, { message: "Field name must not be empty." }),
 
+    text: Zod
+        .string()
+        .min(1, { message: "Field text must not be empty." }),
+
     quiz: Zod
-        .string(),
+        .string()
+        .min(1, { message: "Field quiz must not be empty." }),
     });
 
 export const SectionSearchRequestSchema = Zod.object({
     name: Zod
+        .string()
+        .optional(),
+
+    text: Zod
         .string()
         .optional(),
 
@@ -25,7 +34,12 @@ export const SectionUpdateRequestSchema = Zod.object({
         .min(1, { message: "Field name must not be empty." }),
 
     quiz: Zod
-        .string(),
+        .string()
+        .optional(),
+
+    text: Zod
+        .string()
+        .optional(),
 });
 
 export const SectionRemoveRequestSchema = Zod.object({
